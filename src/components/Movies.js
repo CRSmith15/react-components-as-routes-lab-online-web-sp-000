c0da2d898d67284ecb1f12df1b16df4b1f0eb9da
Chance Smith <chancesmith97@hotmail.com>
2020-10-04 23:52:40 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,13 +5,13 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
-        {movies.map((movie) => 
+        {movies.map((movie) =>
           <div>
             <h3>Title: {movie.title}</h3>
             <h3>Time: {movie.time}</h3>
             <h4>Genres:</h4>
             <ul>
-              {movie.genres.map((genre) => 
+              {movie.genres.map((genre) =>
                 <li>{genre}</li>
         )}
            </ul>
